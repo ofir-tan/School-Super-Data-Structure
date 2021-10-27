69eb0c9818dc3ddcb9fe56a620bd00a25cc53938
Ofir <78694515+ofir-tan@users.noreply.github.com>
2021-10-27 13:13:30 +0300

    Add files via upload

--- source/main.cpp
+++ source/main.cpp
@@ -1,29 +1,20 @@
-#include <string>
 #include <iostream>
-
-using namespace std;
-
-#include <fstream>
-#include <vector>
-#include "Student.h"
-#include "AuxiliaryFunctions.h"
 #include "School.h"
-#include "set"
+using namespace std;
 
 #define PATH (string)"C:/Users/AppTa/Desktop/DataForProject/"
 
 int main(int argc, char *argv[]) {
     School *bgu = School::getInstance(Six, Three);
-    cout << bgu->importStudents(PATH + "Students.txt") << endl;
-    cout << bgu->importWorkers(PATH) << endl;
+
+    bgu->importStudents(PATH + "Students.txt");
+    bgu->importWorkers(PATH);
 
     bgu->printStudents();
     bgu->printWorkers();
 
-
     bgu->topStudent()->info();
     bgu->topWorker()->info();
-
 
     cout << '\n' << bgu->meanGPA() << endl;
     cout << bgu->meanSalary() << endl;
