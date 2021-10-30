3ac9eca855f5d5d81151533f2dc99a2b6d0c789d
Ofir <78694515+ofir-tan@users.noreply.github.com>
2021-10-30 17:30:05 +0300

    Add files via upload

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 #include "School.h"
 
 using namespace std;
@@ -8,13 +10,26 @@ using namespace std;
 int main(int argc, char *argv[]) {
     School *bgu = School::getInstance(Six, Three);
 
-    bgu->importStudents(PATH + "Students.txt");
-    bgu->importWorkers(PATH);
+    auto start = chrono::high_resolution_clock::now();
+
+    // demonstration of several methods:
+    thread t1([bgu](const string &str) { bgu->importStudents(str); }, PATH + "Students.txt");
+    thread t2([bgu](const string &str) { bgu->importWorkers(str); }, PATH);
+    t1.join();
+    t2.join();
 
     bgu->info();
 
     if (bgu->findStudent("Jacki Mickey"))
         bgu->findStudent("Jacki Mickey")->info();
 
+    auto secretary = bgu->getWorkers<Secretary *>();
+    for (auto &s: secretary)
+        s->info();
+
+    // for comparing times with/without threads:
+    auto stop = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    cout << "\nExecution Time: " << duration.count() << endl;
     return 0;
 }
